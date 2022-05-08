bc7916f645eb7d3ccb6acea2f1f0a80652a06fe6
Sascha1403 <s-steger@live.de>
2022-05-08 19:28:40 +0200

    Volume of a Cuboid.py

---
+++ 8_Kyu/Volume of a Cuboid.py
@@ -0,0 +1,6 @@
+"""
+Bob needs a fast way to calculate the volume of a cuboid with three values: length, width and the height of the cuboid.
+Write a function to help Bob with this calculation.
+"""
+def getVolumeOfCobiod(length, width, height):
+    return length * width * height
